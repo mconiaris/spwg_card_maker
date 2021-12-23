945217a60bcd6385b2b59c45fdf2aa8b06f79e8e
mconiaris <mconiaris@gmail.com>
2021-12-23 10:15:05 -0500

    Ran migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_194721) do
+ActiveRecord::Schema.define(version: 2021_12_22_221406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_194721) do
     t.integer "suby"
     t.integer "tzgx"
     t.integer "tagy"
+    t.string "specialty"
   end
 
 end
