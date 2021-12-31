fa1ae611f9e1fcf1b03ea392d1f9f9344a4e5ac2
mconiaris <mconiaris@gmail.com>
2021-12-31 13:17:57 -0500

    Set up basic wrestler helper to generate statistics in real time. It needs work.

--- app/helpers/wrestlers_helper.rb
+++ app/helpers/wrestlers_helper.rb
@@ -1,2 +1,17 @@
 module WrestlersHelper
+	def generate_wrestler_stats(wrestler)
+		stats = wrestler.analyze
+
+		wrestler.tt = stats[:tt_probability]
+		wrestler.card_rating = stats[:total_card_rating]
+		wrestler.oc_prob = stats[:oc_probability]
+ 		wrestler.total_points = stats[:total_card_points]
+ 		wrestler.dq_prob = stats[:dq_probability_per_round]
+ 		wrestler.pa_prob = stats[:pa_probability_per_round]
+ 		wrestler.sub_prob = stats[:sub_probability_per_round]
+ 		wrestler.xx_prob = stats[:xx_probability_per_round]
+ 		# wrestler.submission = stats[]
+ 		# wrestler.tag_team_save = stats[]
+ 		wrestler.save
+	end
 end
