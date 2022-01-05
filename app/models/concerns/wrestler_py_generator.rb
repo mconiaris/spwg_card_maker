6a6f9d5c86bf2d496c13ef910953a115f461daf0
mconiaris <mconiaris@gmail.com>
2022-01-05 15:34:01 -0500

    Created capture_move_name method to isolate just the move name.

--- app/models/concerns/wrestler_py_generator.rb
+++ app/models/concerns/wrestler_py_generator.rb
@@ -22,7 +22,7 @@ module WrestlerPyGenerator
 			file.write("#    1000 = OC\n")
 			file.write("#    1001 = OC/TT\n")
 			file.write("#    1002 = DC\n")
-			file.write("GeneralCard = #{general_card}\n")
+			file.write("GeneralCard = #{make_general_card}\n")
 			file.write("\n")
 			file.write("# Offensive Card Definitions:\n")
 			file.write("#    1003 = Pin attempt move (P/A)\n")
@@ -33,18 +33,19 @@ module WrestlerPyGenerator
 			file.write("#    1009 = Grudge Match Move (XX)\n")
 			file.write("#    1010 = Ropes Move (ROPES)\n")
 			file.write("OffensiveCard = \\\n")
-			file.write("[   {   'MOVE_NAME': 'BACK BODY DROP', 'MOVE_POINTS': 9, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'TURNBUCKLE SMASH', 'MOVE_POINTS': 9, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'TOMAHAWK DROP', 'MOVE_TYPE': 1005},\n")
-			file.write("    {   'MOVE_NAME': 'HIP ROLL', 'MOVE_POINTS': 6, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'TOMAHAWK CHOP', 'MOVE_POINTS': 8, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'BODY SLAM', 'MOVE_POINTS': 7, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'DBLE TOMAHAWK CHOP', 'MOVE_POINTS': 8, 'MOVE_TYPE': 1009},\n")
-			file.write("    {   'MOVE_NAME': 'VERTICAL SUPLEX', 'MOVE_POINTS': 9, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'DBLE UNDERHOOK SUPLEX', 'MOVE_POINTS': 9, 'MOVE_TYPE': 1003},\n")
-			file.write("    {   'MOVE_NAME': 'CHOP TO CHEST', 'MOVE_POINTS': 7, 'MOVE_TYPE': 1008},\n")
-			file.write("    {   'MOVE_NAME': 'ROPES', 'MOVE_TYPE': 1010}]\n")
-			file.write("    \n")
+			make_offensive_card
+			# file.write("[   {   'MOVE_NAME': '#{self[:oc02]}', 'MOVE_POINTS': #{move_points[:oc02_points]}, 'MOVE_TYPE': #{move_type("oc02")}},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc03]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc04]}', 'MOVE_TYPE': 1005},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc05]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc06]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc07]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc08]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1009},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc09]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc10]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1003},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc11]}', 'MOVE_POINTS': #{move_points[, 'MOVE_TYPE': 1008},\n")
+			# file.write("    {   'MOVE_NAME': '#{self[:oc12]}', 'MOVE_TYPE': 1010}]\n")
+			# file.write("    \n")
 			file.write("# Defensive Card Definitions:\n")
 			file.write("#    0 = B - No points on defense\n")
 			file.write("#    2 = A - 2 points on defense\n")
@@ -95,7 +96,7 @@ module WrestlerPyGenerator
 
 		private
 
-		def general_card
+		def make_general_card
 			gc = [ self[:gc02], self[:gc03], self[:gc04], self[:gc05], 
 				self[:gc06], self[:gc07], self[:gc08], self[:gc09], self[:gc10], 
 				self[:gc11], self[:gc12] ]
@@ -108,6 +109,72 @@ module WrestlerPyGenerator
 				else item == "DC"
 					item = 1002
 				end
+			end
+		end
+
+		def make_offensive_card
+
+			oc_move_array = Array.new
+
+			oc_points_hash = move_points.select { |k,v| k.to_s[0..1] == "oc" }
+			oc_moves_hash = attributes.select { |k,v| k[0..1] == "oc" }
+			oc_moves_hash.delete("oc_prob")
+
+
+			oc_moves_hash.each { |k,v|
+				array_key = (k[2..3].to_i) - 2
+
+				if v.underscore == "ropes"
+					oc_move_array[array_key] = "{   'MOVE_NAME': 'ROPES', 'MOVE_TYPE': 1010}"
+				else
+					move = capture_move_name(v.underscore)
+					oc_move_array[array_key] = "{   'MOVE_NAME': '#{move}', 'MOVE_POINTS': 9, 'MOVE_TYPE': 1008}"
+				end
+			}
+			# TODO: This is kind of working, but not quite. Sometimes it does.
+			binding.pry
+		end
+
+		# Just get the move names and not the values
+		def capture_move_name(move)
+
+			if move.include?('(xx)')
+				move.gsub!("(xx)","")
+			elsif move.include?('p/a')
+				move.gsub!("p/a","")
+			elsif move.include?('*')
+				move.gsub!("*","")
+			elsif move.include?('(dq)')
+				move.gsub!("(dq)","")
+			elsif move.include?('(s)')
+				move.gsub!("(s)","")
+			end
+
+			move.gsub!(/\d+/, "")
+			move.strip!
+			move.upcase
+		end
+
+
+		def move_type(roll)
+			types = Hash.new
+
+			move_points.select { |k,v|
+				if k.to_s[0..3] == roll
+					types[k] = v
+				end
+			}
+			# Get points, dq, pa, sub & xx values relatet to roll
+			types_hash = types.select { |m| m == 1 }
+
+			# Determine Move Type
+			if types_hash.size == 0
+				move_type = 1008
+			else
+				move_type = types_hash.select {
+			binding.pry
+
+				}
 			end
 		end
 
