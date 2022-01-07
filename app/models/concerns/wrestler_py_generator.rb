e31c3a4c7e79c5160b0fbd3ba1becc86cb4ba97c
mconiaris <mconiaris@gmail.com>
2022-01-07 09:49:45 -0500

    Fixed error so that Specialty p/a, sub, etc. rolls are written to game card.

--- app/models/concerns/wrestler_py_generator.rb
+++ app/models/concerns/wrestler_py_generator.rb
@@ -150,12 +150,12 @@ module WrestlerPyGenerator
 		def make_specialty_card(file)
 			specialty_move = self["specialty"].upcase
 
-			file.write("Specialty = {   '#{specialty_move}': [   {   'MOVE_POINTS': #{move_points[:s1_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s1])}\},\n")
-			file.write("                         {   'MOVE_POINTS': #{move_points[:s2_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s2])}\},\n")
-			file.write("                         {   'MOVE_POINTS': #{move_points[:s3_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s3])}\},\n")
-			file.write("                         {   'MOVE_POINTS': #{move_points[:s4_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s4])}\},\n")
-			file.write("                         {   'MOVE_POINTS': #{move_points[:s5_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s5])}\},\n")
-			file.write("                         {   'MOVE_POINTS': #{move_points[:s6_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s6])}\}\]\}\n")
+			file.write("Specialty = {   '#{specialty_move}': [   {   'MOVE_POINTS': #{move_points[:s1_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s1].underscore)}\},\n")
+			file.write("                         {   'MOVE_POINTS': #{move_points[:s2_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s2].underscore)}\},\n")
+			file.write("                         {   'MOVE_POINTS': #{move_points[:s3_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s3].underscore)}\},\n")
+			file.write("                         {   'MOVE_POINTS': #{move_points[:s4_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s4].underscore)}\},\n")
+			file.write("                         {   'MOVE_POINTS': #{move_points[:s5_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s5].underscore)}\},\n")
+			file.write("                         {   'MOVE_POINTS': #{move_points[:s6_points]}, 'MOVE_TYPE': #{get_specialty_move_type(self[:s6].underscore)}\}\]\}\n")
 			file.write("\n")
 		end
 
