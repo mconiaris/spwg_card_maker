264d5ed353cdc28ec477941a2c2eaaa4f24e274f
mconiaris <mconiaris@gmail.com>
2022-01-07 08:09:54 -0500

    Permitted the set parameter in the form.

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -46,7 +46,7 @@ class WrestlersController < ApplicationController
   private
 
   def wrestler_params
-    params.require(:wrestler).permit(:name, :gc02, :gc03, :gc04, 
+    params.require(:wrestler).permit(:name, :set, :gc02, :gc03, :gc04, 
       :gc05, :gc06, :gc07, :gc08, :gc09, :gc10, :gc11, :gc12, :dc02, :dc03, 
       :dc04, :dc05, :dc06, :dc07, :dc08, :dc09, :dc10, :dc11, :dc12, :specialty, 
       :s1, :s2, :s3, :s4, :s5, :s6, :subx, :suby, :tagx,:tagy, :prioritys,  
--- app/models/concerns/wrestler_py_generator.rb
+++ app/models/concerns/wrestler_py_generator.rb
@@ -12,7 +12,8 @@ module WrestlerPyGenerator
 
 		def print_card
 
-			file = File.new("#{self[:name]}_#{self[:set]}.py", 'w')
+			file = File.new("#{self[:name]}_#{self[:set]}.txt", 'w')
+			binding.pry
 			file.write("# -*- coding: utf-8 -*-\n")
 			file.write("from data.globalConstants import *\n")
 			file.write("# #{self[:name]}\n")
