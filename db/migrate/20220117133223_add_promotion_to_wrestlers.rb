5e72b0f68646e2b0690d1b4b478f2bc72a1c2ae1
mconiaris <mconiaris@gmail.com>
2022-01-17 08:34:30 -0500

    Allowed promotion to be null in wrestlers.

--- db/migrate/20220117133223_add_promotion_to_wrestlers.rb
+++ db/migrate/20220117133223_add_promotion_to_wrestlers.rb
@@ -1,5 +1,6 @@
 class AddPromotionToWrestlers < ActiveRecord::Migration[7.0]
   def change
     add_column :wrestlers, :promotion, :string
+    change_column_null :wrestlers, :promotion, true
   end
 end
