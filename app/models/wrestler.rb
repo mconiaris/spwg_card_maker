4dd89a34ed8e1ff73579599d5387f05e7ad5c93e
mconiaris <mconiaris@gmail.com>
2022-01-19 11:06:27 -0500

    Created partially working validation for Specialy move values input.

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -22,4 +22,6 @@ class Wrestler < ApplicationRecord
 		message: "%{value} cannot be entered as a DC value. You only use 'A' 'B' 'C' 'Reverse' 'REVERSE'"
 	}
 
+	validates :s1, :s2, :s3, :s4, :s5, :s6, format: { with: /(\d{1,2}|DQ)|(\sP\/A|\*|\s\(XX\))/ }
+
 end
