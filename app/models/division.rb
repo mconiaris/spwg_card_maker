6fd39f4c37a7bf4075a7bc01e4962560592041b6
mconiaris <mconiaris@gmail.com>
2022-02-04 14:11:26 -0500

    Added validations for division model.

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,5 @@
 class Division < ApplicationRecord
 	has_many :wrestlers
+
+	validates :name, inclusion { in: %w( Cruiserweight Legend Midcard Preliminary Regional Super Veteran Women World Young )}
 end
