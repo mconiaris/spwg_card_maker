10c4b3aebe7d7c1fd728b31614749df27bcf2a0c
mconiaris <mconiaris@gmail.com>
2022-02-07 13:18:05 -0500

    CRUD for division.

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,2 +1,51 @@
 class DivisionsController < ApplicationController
+
+	def index
+    @divisions = Division.all
+  end
+
+  def show
+    @division = Division.find(params[:id])
+  end
+
+  def new
+    @division = Division.new
+  end
+
+  def create
+    @division = Division.new(division_params)
+
+    if @division.save
+      redirect_to @division
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @division = Division.find(params[:id])
+  end
+
+  def update
+    @division = Division.find(params[:id])
+                                  
+    if @division.update(division_params)
+      redirect_to @division
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @division = Division.find(params[:id])
+    @division.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def division_params
+    params.require(:division).permit(:name)
+  end
 end
