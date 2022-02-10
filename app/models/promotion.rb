057e8872b18947c2f7fb025bff3a838b29bf3f6f
mconiaris <mconiaris@gmail.com>
2022-02-10 09:24:41 -0500

    Fixed validation errors in promotion model.

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,5 @@
 class Promotion < ApplicationRecord
 	has_many :wrestlers
 
-	validates :name, inclusion: { in: %W( AWA Florida Georgia Memphis #{Mid Atlantic} #{Mid South} None NWA Southwest #{World Class} Women WWC WWF )}
+	validates :name, inclusion: { in: %W( AWA Florida Georgia Memphis #{"Mid Atlantic"} #{"Mid South"} None NWA Southwest #{"World Class"} Women WWC WWF )}
 end
