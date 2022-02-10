6a2ff3b41753a247e3c7d960eece00a0ad4f1233
mconiaris <mconiaris@gmail.com>
2022-02-10 10:02:21 -0500

    Filled out promotion controller.

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,2 +1,50 @@
 class PromotionsController < ApplicationController
+	def index
+    @promotions = Promotion.all
+  end
+
+  def show
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def update
+    @promotion = Promotion.find(params[:id])
+                                  
+    if @promotion.update(promotion_params)
+      redirect_to @promotion
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @promotion = Promotion.find(params[:id])
+    @promotion.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:name)
+  end
 end
