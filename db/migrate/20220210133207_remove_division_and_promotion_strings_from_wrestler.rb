b2dcfd5b28d3749e6645ae96d0641b3e2ea91268
mconiaris <mconiaris@gmail.com>
2022-02-10 08:34:32 -0500

    Filled out logic for migration.

--- db/migrate/20220210133207_remove_division_and_promotion_strings_from_wrestler.rb
+++ db/migrate/20220210133207_remove_division_and_promotion_strings_from_wrestler.rb
@@ -1,4 +1,7 @@
 class RemoveDivisionAndPromotionStringsFromWrestler < ActiveRecord::Migration[7.0]
   def change
+    change_table :wrestlers do |t|
+      t.remove :division, :promotion
+    end
   end
 end
