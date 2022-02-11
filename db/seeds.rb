a684736ecebd0e6d12bb13d882b709e3c1b5449a
mconiaris <mconiaris@gmail.com>
2022-02-11 11:36:49 -0500

    Revised seed file to create Division and Promotion objects.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'csv'
 
+Divisions.destroy_all
+Promotions.destroy_all
 Wrestler.destroy_all
+
+promotions = [ "AWA", "Florida", "Georgia", "Memphis", "Mid Atlantic", 
+  "Mid South", "None", "NWA", "Southwest", "World Class", "Women", "WWC", 
+  "WWF" ]
+
+ promotions.each { |p| Promotions.create(p) }
+
+
+divisions = [ "Cruiserweight", "Legend", "Midcard", "None", "Preliminary",
+  "Regional Champion", "Super Heavyweight", "Veteran", "Women",
+  "World Champion", "Young Star" ]
+
+divisions.each { |d| Division.create(d) }
+
 
 CSV.foreach(Rails.root.join('lib/seed_file.csv'), headers: true, header_converters: :symbol) do |row|
   
