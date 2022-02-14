8119eae4981350ef9beca6fdb237a6c5be8a2491
mconiaris <mconiaris@gmail.com>
2022-02-14 13:53:11 -0500

    Deleted main controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'main/home'
-
-  get 'main/home'
-  get 'main/about'
+  root 'wrestlers#index'
 
   resources :wrestlers
 
