b86ac24d4ad9e81aade064e691621c1557df5169
mconiaris <mconiaris@gmail.com>
2022-02-17 17:15:28 -0500

    Edited routes to limit user actions. Removed edit link on show page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
 
-  resources :wrestlers
-  resources :divisions
-  resources :promotions
+  resources :wrestlers, only: [:new, :index, :edit, :show, :create ]
+  resources :divisions, only: [:index, :show ]
+  resources :promotions, only: [:index, :show ]
 
   get 'wrestlers/:id/dup_wrestler', to: 'wrestlers#dup_wrestler', as: :dup_wrestler
 end
