adcd53cbfdbb8ffe9b43c0c11a5ef4d6683cc25b
mconiaris <mconiaris@gmail.com>
2022-02-22 13:03:08 -0500

    Added a controller and index view to display tag team experts based on their tag teaming or save probability. Wrestler card did not need to have both.

--- app/controllers/tag_team_wrestlers_controller.rb
+++ app/controllers/tag_team_wrestlers_controller.rb
@@ -1,4 +1,26 @@
 class TagTeamWrestlersController < ApplicationController
+
+  @@sort_order = "ASC"
+
+  # TODO: Explore how this works and make it cleaner.
   def index
+    if params[:sort] == nil
+      @wrestlers = Wrestler.where("tt > ? or tag_team_save > ?", 13.0, 42.0).order(tag_team_save: :desc)
+    else
+      @wrestlers = Wrestler.where("tt > ? or tag_team_save > ?", 13.0, 42.0)
+      @wrestlers = sort_wrestler_index
+    end
+  end
+
+  private
+
+  def sort_wrestler_index
+    if @@sort_order == "ASC"
+      @@sort_order = "DESC"
+      @wrestlers = Wrestler.where("tt > ? or tag_team_save > ?", 13.0, 42.0).order(params[:sort])
+    else
+      @@sort_order = "ASC"
+      @wrestlers = Wrestler.where("tt > ? or tag_team_save > ?", 13.0, 42.0).order("#{params[:sort]} DESC")
+    end
   end
 end
