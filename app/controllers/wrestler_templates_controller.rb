812a062260864d8c7effb4f00b6f2378add08264
mconiaris <mconiaris@gmail.com>
2022-03-01 15:41:10 -0500

    Created 8% option for OC template.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -7,6 +7,10 @@ class WrestlerTemplatesController < ApplicationController
       @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC", gc04: "DC", gc05: "DC", 
             gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
             gc10: "DC", gc11: "DC", gc12: "OC" })
+    when "8"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "OC", gc04: "DC", gc05: "DC", 
+            gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
+            gc10: "DC", gc11: "DC", gc12: "OC" })
     else
       @wrestler = Wrestler.new
     end
