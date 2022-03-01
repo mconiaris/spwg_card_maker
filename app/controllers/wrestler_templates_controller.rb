a37ceba05fdc41554f3759cb76b5e7d0fba0dece
mconiaris <mconiaris@gmail.com>
2022-03-01 15:35:01 -0500

    Added option to create new wrestler from an OC template. Modified navbar to reflect changes.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -1,5 +1,16 @@
 class WrestlerTemplatesController < ApplicationController
+
+  # TODO: DRY this up.
   def new
-    @wrestler = Wrestler.new({gc02: "OC"})
+    if params[:oc] == "6"
+      @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC", gc04: "DC", gc05: "DC", 
+            gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
+            gc10: "DC", gc11: "DC", gc12: "OC" })
+    else
+      @wrestler = Wrestler.new
+    end
   end
+
+  private
+
 end
