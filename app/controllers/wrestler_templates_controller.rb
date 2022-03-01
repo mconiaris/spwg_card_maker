3e05b54fef50d0efd2a30107b204828d7a39f155
mconiaris <mconiaris@gmail.com>
2022-03-01 15:49:05 -0500

    Created oc template options for 11 & 13%.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -4,13 +4,25 @@ class WrestlerTemplatesController < ApplicationController
   def new
     case params[:oc]
     when "6"
-      @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC", gc04: "DC", gc05: "DC", 
-            gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
-            gc10: "DC", gc11: "DC", gc12: "OC" })
+      @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC", 
+        gc04: "DC", gc05: "DC", gc06: "DC", gc07: "DC", 
+        gc08: "DC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
     when "8"
-      @wrestler = Wrestler.new({ gc02: "DC", gc03: "OC", gc04: "DC", gc05: "DC", 
-            gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
-            gc10: "DC", gc11: "DC", gc12: "OC" })
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "OC", 
+        gc04: "DC", gc05: "DC", gc06: "DC", gc07: "DC",
+        gc08: "DC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
+    when "11"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "OC", gc05: "DC", gc06: "DC", gc07: "DC",
+        gc08: "DC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
+    when "13"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "DC", gc07: "DC",
+        gc08: "DC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
     else
       @wrestler = Wrestler.new
     end
