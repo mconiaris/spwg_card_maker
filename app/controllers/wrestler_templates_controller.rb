f4f6325ef81ab93e932538921f4ab17b5c09ffc0
mconiaris <mconiaris@gmail.com>
2022-03-01 15:38:08 -0500

    Changed if statement in templates controller to case statement.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -2,7 +2,8 @@ class WrestlerTemplatesController < ApplicationController
 
   # TODO: DRY this up.
   def new
-    if params[:oc] == "6"
+    case params[:oc]
+    when "6"
       @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC", gc04: "DC", gc05: "DC", 
             gc06: "DC", gc07: "DC", gc08: "DC", gc09: "DC", 
             gc10: "DC", gc11: "DC", gc12: "OC" })
