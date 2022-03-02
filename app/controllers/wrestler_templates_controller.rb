e5b1a11bbf5dc883324bb5a4dfb73e603827caa1
mconiaris <mconiaris@gmail.com>
2022-03-02 08:58:15 -0500

    Fixed math for 33% option. Created 36% option.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -54,7 +54,12 @@ class WrestlerTemplatesController < ApplicationController
         gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
         gc12: "DC" })
     when "33"
-      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+      @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "DC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "36"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "OC",
         gc04: "DC", gc05: "DC", gc06: "DC", gc07: "OC",
         gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
         gc12: "DC" })
