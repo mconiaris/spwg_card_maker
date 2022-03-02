0cc4d9e4ebed3e121f415178a778f15faeef856f
mconiaris <mconiaris@gmail.com>
2022-03-02 09:17:30 -0500

    Added other OC templates and links.

--- app/controllers/wrestler_templates_controller.rb
+++ app/controllers/wrestler_templates_controller.rb
@@ -63,6 +63,76 @@ class WrestlerTemplatesController < ApplicationController
         gc04: "DC", gc05: "DC", gc06: "DC", gc07: "OC",
         gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
         gc12: "DC" })
+    when "39"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "OC", gc05: "DC", gc06: "DC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "42"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "DC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "44"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "47"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
+    when "50"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "DC", gc11: "OC", 
+        gc12: "DC" })
+    when "53"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "DC", gc10: "OC", gc11: "DC", 
+        gc12: "DC" })
+    when "56"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "58"
+      @wrestler = Wrestler.new({ gc02: "OC", gc03: "DC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "61"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "OC",
+        gc04: "DC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "64"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "OC", gc05: "DC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "67"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "DC" })
+    when "69"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "DC", 
+        gc12: "OC" })
+    when "72"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "DC", gc11: "OC", 
+        gc12: "DC" })
+    when "75"
+      @wrestler = Wrestler.new({ gc02: "DC", gc03: "DC",
+        gc04: "DC", gc05: "OC", gc06: "OC", gc07: "OC",
+        gc08: "OC", gc09: "OC", gc10: "OC", gc11: "DC", 
+        gc12: "DC" })
     else
       @wrestler = Wrestler.new
     end
