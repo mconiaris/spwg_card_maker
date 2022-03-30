72192f3351f297ba99d966b9f0c95568230a6fd5
mconiaris <mconiaris@gmail.com>
2022-03-30 08:29:55 -0400

    Revised how oc_probabiity is presented into a view. This will need to be refactored further.

--- app/models/concerns/wrestler_stats.rb
+++ app/models/concerns/wrestler_stats.rb
@@ -10,18 +10,18 @@ module WrestlerStats
 
   def set_wrestler_stats
 
-    stats = self.analyze
+    self.analyze
 
-    self.tt = stats[:tt_probability]
-    self.card_rating = stats[:total_card_rating]
+    # self.tt = stats[:tt_probability]
+    # self.card_rating = stats[:total_card_rating]
     self.oc_prob = oc_probability
-    self.total_points = stats[:total_card_points_per_round]
-    self.dq_prob = stats[:dq_probability_per_round]
-    self.pa_prob = stats[:pa_probability_per_round]
-    self.sub_prob = stats[:sub_probability_per_round]
-    self.xx_prob = stats[:xx_probability_per_round]
-    self.submission = stats[:submission]
-    self.tag_team_save = stats[:tag_team_save]
+    # self.total_points = stats[:total_card_points_per_round]
+    # self.dq_prob = stats[:dq_probability_per_round]
+    # self.pa_prob = stats[:pa_probability_per_round]
+    # self.sub_prob = stats[:sub_probability_per_round]
+    # self.xx_prob = stats[:xx_probability_per_round]
+    # self.submission = stats[:submission]
+    # self.tag_team_save = stats[:tag_team_save]
 
   end
 
