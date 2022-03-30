4d163c9e3f6d3118648c62b62ac522c8f13c405c
mconiaris <mconiaris@gmail.com>
2022-03-30 12:57:30 -0400

    Deleted commented out old code.

--- app/models/concerns/wrestler_stats.rb
+++ app/models/concerns/wrestler_stats.rb
@@ -9,20 +9,7 @@ module WrestlerStats
 
 
   def set_wrestler_stats
-
     self.analyze
-
-    # self.tt = stats[:tt_probability]
-    # self.card_rating = stats[:total_card_rating]
-    # self.oc_prob = oc_probability
-    # self.total_points = stats[:total_card_points_per_round]
-    # self.dq_prob = stats[:dq_probability_per_round]
-    # self.pa_prob = stats[:pa_probability_per_round]
-    # self.sub_prob = stats[:sub_probability_per_round]
-    # self.xx_prob = stats[:xx_probability_per_round]
-    # self.submission = stats[:submission]
-    # self.tag_team_save = stats[:tag_team_save]
-
   end
 
 end
