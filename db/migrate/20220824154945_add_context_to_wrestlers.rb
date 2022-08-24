64d0e6dbe3fd9424b8f293bff5312251b962b4b2
mconiaris <mconiaris@gmail.com>
2022-08-24 11:56:25 -0400

    Created migration for wrestler context attributes.

--- db/migrate/20220824154945_add_context_to_wrestlers.rb
+++ db/migrate/20220824154945_add_context_to_wrestlers.rb
@@ -1,4 +1,8 @@
 class AddContextToWrestlers < ActiveRecord::Migration[7.0]
   def change
+    add_column :wrestlers, :full_name, :string
+    add_column :wrestlers, :era, :string
+    add_column :wrestlers, :year, :integer
+    add_column :wrestlers, :position, :string
   end
 end
