05da8c7f0e173ed235d9c060e07465bece95bcb0
mconiaris <mconiaris@gmail.com>
2022-08-24 13:16:14 -0400

    Revised promotion list to include Misc and remove WWC & Women.

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,5 @@
 class Promotion < ApplicationRecord
 	has_many :wrestlers
 
-	validates :name, inclusion: { in: %W( AWA Florida Georgia Memphis #{"Mid Atlantic"} #{"Mid South"} None NWA Southwest #{"World Class"} Women WWC WWF )}
+	validates :name, inclusion: { in: %W( AWA Florida Georgia Memphis #{"Mid Atlantic"} #{"Mid South"} Misc None NWA Southwest #{"World Class"} WWF )}
 end
