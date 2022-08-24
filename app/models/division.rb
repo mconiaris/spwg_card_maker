284b9547f8287d1bb4cc574e38c7068831584922
mconiaris <mconiaris@gmail.com>
2022-08-24 13:22:26 -0400

    Revised division validation to allow 'Heavyweight'

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,5 +1,5 @@
 class Division < ApplicationRecord
 	has_many :wrestlers
 
-	validates :name, inclusion: { in: %W( Cruiserweight Fake None #{"Super Heavyweight"} Veteran Women #{"Young Star"} )}
+	validates :name, inclusion: { in: %W( Cruiserweight Fake Heavyweight None #{"Super Heavyweight"} Veteran Women #{"Young Star"} )}
 end
