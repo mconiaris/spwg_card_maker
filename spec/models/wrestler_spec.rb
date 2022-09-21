25abe0c9d9ef42deb74956dc53a3b8d8778e3462
mconiaris <mconiaris@gmail.com>
2022-09-21 16:20:53 -0400

    Created outline of rspec tests.

--- spec/models/wrestler_spec.rb
+++ spec/models/wrestler_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Wrestler, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "is not valid without a name"
+  it "is not valid without a set"
+  it "is not valid without a tt"
+  it "is not valid without a card rating"
 end
