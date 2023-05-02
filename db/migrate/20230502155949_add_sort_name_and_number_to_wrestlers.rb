2007cf5fbbdfc612301b9947852764ac256540b7
mconiaris <mconiaris@gmail.com>
2023-05-02 12:05:55 -0400

    Added sort_name and card_number to Wrestlers through a Active Record migration. Revised seed file.

--- db/migrate/20230502155949_add_sort_name_and_number_to_wrestlers.rb
+++ db/migrate/20230502155949_add_sort_name_and_number_to_wrestlers.rb
@@ -1,4 +1,8 @@
 class AddSortNameAndNumberToWrestlers < ActiveRecord::Migration[7.0]
   def change
+    remove_column :wrestlers, :full_name, :string
+
+    add_column :wrestlers, :sort_name, :string
+    add_column :wrestlers, :card_number, :string
   end
 end
