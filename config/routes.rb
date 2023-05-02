04c31b3eb98debd25df497927785b5b5d80f4b32
mconiaris <mconiaris@gmail.com>
2023-05-02 11:51:37 -0400

    Changed root to wrestlers index page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   # Defines the root path route ("/")
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'wrestlers#index'
   
   get 'welcome/index'
   get 'welcome/about'
