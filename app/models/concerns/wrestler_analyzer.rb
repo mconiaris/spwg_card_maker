80247514a659d63c6baad81a66a32fde0dd9e783
mconiaris <mconiaris@gmail.com>
2023-08-22 13:34:37 -0400

    Revised total card rating formula.

--- app/models/concerns/wrestler_analyzer.rb
+++ app/models/concerns/wrestler_analyzer.rb
@@ -421,11 +421,13 @@ module WrestlerAnalyzer
 		# Double P/A per round and divide XX per round for total card value
 		# to increase relative value of pin attempts.
 		@total_card_values = points_per_round + 
-			dq_probability_per_round + (pa_probability_per_round * 2) +
-				sub_probability_per_round + (xx_probability_per_round / 2)
+			(dq_probability_per_round * 10) + 
+			(pa_probability_per_round * 20) +
+			(sub_probability_per_round * 15) + 
+			(xx_probability_per_round * 5)
 
 		@total_card_rating = total_card_values + 
-			singles_priority - submission_loss_probabilty
+			singles_priority - (submission_loss_probabilty * 10)
 
 		return total_card_rating
 	end
